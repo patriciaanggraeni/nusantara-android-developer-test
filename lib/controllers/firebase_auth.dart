05f577de3209c77c9e659037a950f4c531ee0ed3
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2023-12-29 22:06:37 +0700

    create firebase auth controller class and make auth for user

---
+++ lib/controllers/firebase_auth.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:nusantara_android_developer_test/screens/home.dart';
+import 'package:nusantara_android_developer_test/screens/login.dart';
+import 'package:nusantara_android_developer_test/screens/register.dart';
+
+class FirebaseAuthController extends GetxController {
+
+  static final FirebaseAuthController _instance = Get.find();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  late Rx<User?> _user;
+
+  @override
+  void onReady() {
+    _user = Rx<User?>(_auth.currentUser);
+    _user.bindStream(_auth.userChanges());
+    ever(_user, _initial);
+    super.onReady();
+  }
+
+  FirebaseAuth get auth => _auth;
+  FirebaseAuthController get instance => _instance;
+
+  _initial(User? user) => user == null ? 
+    Get.offAll( () => const Login() ) : 
+    Get.offAll( () => const Home() );
+
+}
