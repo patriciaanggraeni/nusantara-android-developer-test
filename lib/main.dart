d1bf75597a06df5aa182ca19d0f072659c74ac0f
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2023-12-29 16:25:17 +0700

    call register screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:nusantara_android_developer_test/firebase_options.dart';
+import 'package:nusantara_android_developer_test/screens/register.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,35 +18,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text(
-            "Home Page",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 25,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        body: const SafeArea(
-          child: Center(
-            child: Text(
-              "Home Page",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: const Register(),
     );
   }
 }
