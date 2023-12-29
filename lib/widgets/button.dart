d80c968d8bdec81477a08415b0515022393d504b
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2023-12-29 16:23:33 +0700

    create button widget

---
+++ lib/widgets/button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:nusantara_android_developer_test/utils/font.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.text, required this.callback});
+
+  final String text;
+  final Function() callback;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: callback,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.blue,
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: Util.myTextStyle(
+              color: Colors.white,
+              size: 15,
+              weight: FontWeight.bold
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
