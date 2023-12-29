febc4d5280374ea6158a271a535b2fdebc883aa7
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2023-12-29 22:15:14 +0700

    call Get.put() function to put FirebaseAuthController class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:nusantara_android_developer_test/controllers/firebase_auth_controller.dart';
 import 'package:nusantara_android_developer_test/firebase_options.dart';
 import 'package:nusantara_android_developer_test/screens/register.dart';
 
@@ -8,9 +9,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
+  ).then( (value) => Get.put(FirebaseAuthController()) );
   runApp(const MyApp());
-}
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
