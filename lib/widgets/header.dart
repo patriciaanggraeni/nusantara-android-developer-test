5f73c319d2839aacd8cc43d69d03da998a16606e
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2023-12-29 16:23:59 +0700

    create header widget

---
+++ lib/widgets/header.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:nusantara_android_developer_test/utils/font.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key, required this.textOne, required this.textTwo});
+
+  final String textOne;
+  final String textTwo;
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth =  MediaQuery.of(context).size.width;
+
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              textOne,
+              style: Util.myTextStyle(
+                size: screenWidth > 600 ? 25 : 20, 
+                weight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              textTwo,
+              style: Util.myTextStyle(
+                size: screenWidth > 600 ? 20 : 15,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
